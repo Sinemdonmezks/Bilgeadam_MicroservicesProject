ebe25fefe285160be36d2bb8c84255d0b44dffdb
Sinemdonmezks <s.k.donmez123@gmail.com>
2022-10-24 15:44:56 +0300

    api-gateway service oluşturuldu. CircuitBreaker -Zipkin bağımlılıkları eklendi.

--- auth-service/src/main/java/com/sinem/controller/AuthController.java
+++ auth-service/src/main/java/com/sinem/controller/AuthController.java
@@ -3,17 +3,14 @@ package com.sinem.controller;
 import com.sinem.dto.request.LoginRequestDto;
 import com.sinem.dto.request.RegisterRequestDto;
 import com.sinem.repository.entity.Auth;
-import com.sinem.repository.enums.Activated;
-import com.sinem.repository.enums.Roles;
 import com.sinem.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.util.List;
 
 import static com.sinem.constants.ApiUrls.*;
 
@@ -35,5 +32,9 @@ public class AuthController {
         if(authService.save(dto))
             return ResponseEntity.ok().build();
         return ResponseEntity.badRequest().build();
+    }
+    @GetMapping("/getall")
+    public ResponseEntity<List<Auth>> getList(){
+        return ResponseEntity.ok(authService.findAll());
     }
 }
